60b0105164868124cdff5b448ec6d2abf71c0301
jrew <it.jrew@gmail.com>
2014-07-29 23:07:39 +0300

    Added a couple of empty files in order to cimmit directore structure.

---
+++ src/main/java/com/jrew/lab/guesscountry/controller/Controller/Controller.java
@@ -0,0 +1,10 @@
+package com.jrew.lab.guesscountry.controller.Controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vadim
+ * Date: 7/29/2014
+ * Time: 11:04 PM
+ */
+public class Controller {
+}
